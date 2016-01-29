875565f6fa3ae9918325c51304919217389505e5
Noah Wimmer <nwimmer123@yahoo.com>
2016-01-29 11:01:52 -0800

    created session and nav bar routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   get 'users/index'
   get '/signup', to: 'users#new', as: 'new_user'
   get 'users/edit'
-  get '/users/:id', to: 'user#show', as: 'user_path'
+  get '/users/:id', to: 'user#show', as: 'user'
 
 
-  get '/login', to: 'sessions#new'
-  get '/logout', to: 'sessions#destroy', as: 'destroy_session_path' 
+  get '/login', to: 'sessions#new', as: 'new_session'
+  get '/logout', to: 'sessions#destroy', as: 'destroy_session' 
 end
