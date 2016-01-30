8661761b292d678cf7a5d2259e150b4a847bf9b0
Noah Wimmer <nwimmer123@yahoo.com>
2016-01-30 13:55:32 -0800

    added user and categories to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  user = User.create([
+    { email: 'a@a.com', first_name: 'a', last_name: 'a', current_city: 'a', password_digest: 'a'},
+    { email: 'b@b.com', first_name: 'b', last_name: 'b', current_city: 'b', password_digest: 'b'},
+    { email: 'c@c.com', first_name: 'c', last_name: 'c', current_city: 'c', password_digest: 'c'},
+    { email: 'nwimmer123@yahoo.com', first_name: 'Noah', last_name: 'Wimmer', current_city: 'Berkeley', password_digest: '123'}
+    ])
+
+  categories = ([{name: 'Tutoring'}, {name: 'Outdoors'}, {name: 'Health and Beauty'}])
+  
