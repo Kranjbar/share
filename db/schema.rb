3951154f2ee8c20426fb2b5ae41402c1820bc237
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-30 12:08:29 -0800

    Made category to skills relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130194454) do
+ActiveRecord::Schema.define(version: 20160130200715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "skills", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+  end
+
+  add_index "skills", ["category_id"], name: "index_skills_on_category_id", using: :btree
 
   create_table "user_skills", force: :cascade do |t|
     t.integer  "user_id"
@@ -46,6 +55,7 @@ ActiveRecord::Schema.define(version: 20160130194454) do
     t.datetime "avatar_updated_at"
   end
 
+  add_foreign_key "skills", "categories"
   add_foreign_key "user_skills", "skills"
   add_foreign_key "user_skills", "users"
 end
