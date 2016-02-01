01339e7f183910da50fb5f4a4a956030dabd27b8
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-02-01 13:30:55 -0800

    Commented wrong controller method out

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -4,13 +4,13 @@ class UserSkillsController < ApplicationController
     @categories = Category.all
   end
 
-  def create
-    @user = current_user
-    @library = Library.find(params[:library_id])
+  # def create
+  #   @user = current_user
+  #   @library = Library.find(params[:library_id])
 
-    @user.libraries.push(@library)
+  #   @user.libraries.push(@library)
 
-    redirect_to "/users/#{@user.id}/libraries"
-  end
+  #   redirect_to "/users/#{@user.id}/libraries"
+  # end
 
 end
