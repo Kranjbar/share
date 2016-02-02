67bd44eb7ce4e99363cb4b0332100a79ebc889c6
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2016-02-02 15:30:06 -0800

    Added bio section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city,  :avatar)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city,  :avatar, :bio)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202202750) do
+ActiveRecord::Schema.define(version: 20160202225821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20160202202750) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "slug"
+    t.text     "bio"
   end
 
   add_index "users", ["slug"], name: "index_users_on_slug", unique: true, using: :btree
