10f409ed3012e05031c5ad3844a33f77c5cf0517
kehontas <kehontas@gmail.com>
2016-02-03 11:23:28 -0800

    add skill to review

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203182146) do
+ActiveRecord::Schema.define(version: 20160203191423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160203182146) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "rskill"
   end
 
   add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
